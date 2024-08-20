380441bec4c3ab46a28859a64694e6ec4a48d209
ホエン <r_rhoen@g-wise.co.jp>
2024-08-20 13:29:40 +0900

    first commit: - installed dependencies - create datepicker

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-container">
+    <CustomCalendar />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CustomCalendar from './components/CustomCalendar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CustomCalendar
   }
 }
 </script>
@@ -19,8 +20,14 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.app-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex; 
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/CustomCalendar.vue
@@ -0,0 +1,195 @@
+<template>
+    <div class="calendar-container">
+        <div class="header d-flex justify-content-between align-items-center">
+            <div><button class="nav-icon" @click="previousMonth">&lt;</button></div>
+            <div class="calendar-year-month">{{ currentDate.format('YYYY年MM月')}}</div>
+            <div><button class="nav-icon" @click="nextMonth">&gt;</button></div>
+        </div>
+        <div class="calendar w-100 mt-2">
+            <div v-for="weekday in weekdays" :key="weekday">
+                <span>{{ weekday }}</span>
+            </div>
+            <div v-for="day, index in monthDays" :key="day" >
+                <span 
+                @click="setSelected(day)"
+                :class="{
+                    'calendar-days': day.isValid(),
+                    'today': isToday(day),
+                    'sunday': isSunday(index),
+                    'saturday': isSaturday(index),
+                    'selected-date': isSelected(day)
+                }">{{ day.isValid() ? day.format('D') : '' }}</span>
+            </div>
+        </div>
+        <span>{{ format(selectedDate) }}</span>
+        <div class="time-picker">
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+import moment from 'moment'
+import 'moment/locale/ja';  // Import Japanese locale data
+
+// Set the default locale to Japanese
+moment.locale('ja');
+
+export default {
+    props: {
+        // modelValue follows vue3 nomenclature for bind model props
+        // expects a date as string
+        modelValue: {
+            type: String,
+            default: moment().format()
+        }
+
+    },
+    data(){
+        return {
+            dateFormat: 'YYYY年M月D日 HH:mm',
+            weekdays: ['日', '月', '火', '水', '木', '金', '土'],
+            monthDays: [],
+            today: moment(),
+            currentDate: moment(),
+            selectedDate: null, // this will later become the iptValue, using v-model
+        }
+    },
+    methods: {
+        getDayOfWeek(date=""){
+            return moment().day(date)
+        },
+        getMonth(date=""){
+           return moment().month(date) 
+        },
+        getYear(date=""){
+            return moment().month(date)
+        },
+    
+        setSelected(day) {
+            if (day.isValid()) {
+                this.selectedDate = day.clone();
+            }
+        },
+        isSelected(day) {
+            return this.selectedDate && day.isValid() && day.isSame(this.selectedDate, 'day');
+        },
+        nextMonth(){
+            this.currentDate.add(1, 'month')
+            this.fillCalendar()
+        },
+        previousMonth(){
+            this.currentDate.subtract(1, 'month')
+            this.fillCalendar()
+        },
+        selectMonth(){},
+        selectYear(){},
+
+        isToday(day) {
+            if (!day) return false
+            const today = moment()
+            return today.date() === day.date()
+        },
+        isSunday(index) {
+            return (index % 7 === 0) && this.monthDays[index] !== ''
+        },
+        isSaturday(index) {
+            return (index % 7 === 6) && this.monthDays[index] !== ''
+        },
+        firstDayAsWeekday() {
+            return this.currentDate.clone().date(1).day()
+        },
+        // createCalendar() {
+        //     const today = moment().format("D");
+        //     const lastDay = ;
+        //     console.log(today)
+        //     console.log(moment().date(1).day())
+        //     console.log(moment().day())
+        // },
+        fillCalendar(){
+            this.monthDays = []
+            let result = []
+            const lastDayMonth = parseInt(this.currentDate.clone().endOf('month').format("D"))
+            const firstWeekDay = this.firstDayAsWeekday()
+
+            // fill blanks until first day of the month
+            for(let j=0; j<firstWeekDay; j++){
+                result.push(moment(null))
+            }
+
+            // fill the days of the month
+            for(let i=1; i <= lastDayMonth; i++){
+                const thisDate = this.currentDate.clone().date(i);
+                result.push(thisDate)
+            }
+
+            this.monthDays = result
+        },
+        format(value) {
+            if (!value) return '';  // Handle null or undefined values
+            const m = moment(value);  // No format string needed for ISO 8601
+            return m.isValid() ? m.format(this.dateFormat) : '';
+        },
+    },
+    computed: {
+        formatedCurrentDate(){
+            return this.currentDate.format('YYYY年MM月')
+        },
+        iptValue: {
+            get () {
+                return this.modelValue
+            },
+            set (newVal) {
+                console.log(newVal)
+                const m = moment(newVal)
+                const date = m.isValid() ? this.format(newVal) : ''
+                this.$emit('input', date)
+            }
+        }
+    },
+    mounted(){
+        moment.locale('ja')
+        this.fillCalendar(this.today)
+    },
+
+}
+</script>
+
+
+<style scoped>
+.calendar-container {
+    width: 400px;
+    min-height: 300px;
+    border: 1px solid black;
+    padding: 8px;
+} 
+
+.calendar {
+    display: grid;
+    text-align: center;
+    grid-template-columns: repeat(7, 1fr);
+    gap: 1rem;
+}
+.calendar-days,
+.calendar-year-month {
+    cursor: pointer;
+}
+
+.sunday {
+    color: red;
+}
+
+.saturday {
+    color: blue;
+}
+
+.today {
+    background: blue;
+    color: white;
+}
+
+.selected-date {
+    background: orange;
+}
+</style>
