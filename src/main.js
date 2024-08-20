50a48226d955f1e1adefdc014fb48aa5b17af473
ホエン <r_rhoen@g-wise.co.jp>
2024-08-20 13:46:26 +0900

    add fonts, and moment locale

--- src/main.js
+++ src/main.js
@@ -20,4 +20,13 @@ import 'moment/locale/ja';  // Import Japanese locale data
 // Set the default locale to Japanese
 moment.locale('ja');
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faChevronLeft, faChevronRight)
+
+// In your Vue app setup
+app.component('font-awesome-icon', FontAwesomeIcon)
+
 app.mount('#app')
